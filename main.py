f78bf7f30711f29792b0dfaf5bdead152c90ebf5
Itay Dali <itaydali@gmail.com>
2022-03-24 14:45:35 +0200

    python version.

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import copy
+from time import sleep
+
+#Author: Itay Dali
+
+
+SIGN='@'
 
 def build_empty_grid(size):
     """
@@ -17,6 +23,10 @@ def build_empty_grid(size):
 def getPoint(point):
     return point[0],point[1]
 
+def check_range(grid,point):
+    size = len(grid)
+    x,y=getPoint(point)
+    return x>=0 and x<size and y>=0 and y<size
 def build_grid(size,startPositions):
     """
 
@@ -41,18 +51,63 @@ def check_neighbors(grid,point):
     for row in range(-1,2):
         for col in range(-1,2):
             if row ==0 and col ==0 : continue
+            if not check_range(grid,(col+x,row+y)) : continue
             if grid[y+row][x+col]:
                 neighbors+=1
     return neighbors
 
 
-def turn(grid):
+def play_turn(grid):
     size = len(grid)
-    newGrid = build_empty_grid(size)
+    newGrid =copy.deepcopy(grid)
     for row in range(size):
         for col in range(size):
             neighbors = check_neighbors(grid,(col,row))
+            if grid[row][col] and (neighbors!=2 or neighbors!=3):
+                newGrid[row][col] = False
+            if (not grid[row][col]) and  neighbors==3:
+                newGrid[row][col] = True
+    return newGrid
+
+def print_grid(grid,turn):
+    print("*******************")
+    print()
+    print("turn number:",turn)
+    print()
+    size = len(grid)
+    print('_'*(size+2))
+    for row in range(size):
+        print('|',end='')
+        for col in range(size):
+            if grid[row][col]: print(SIGN,end='')
+            else : print(' ',end='')
+        print('|')
+    print('_' * (size + 2))
+    print()
+    print("*******************")
+
+def check_still_alive(grid):
+    size = len(grid)
+    for row in range(size):
+        for col in range(size):
+            if grid[row][col]: return True
+    return False
+
+def play_game(turns):
+    startPositions = [(9,9),(10,9),(10,10),(8,8),(7,8),(30,31),(30,32),(30,33),(32,31),(30,35),(32,33),(34,31),(34,32),(34,33)]
+    size = 50
+    DELAY = 1
+    grid = build_grid(size,startPositions)
+    print_grid(grid,0)
+    for turn in range(1,turns+1):
+        grid = play_turn(grid)
+        if check_still_alive(grid):
+            print_grid(grid,turn)
+            sleep(DELAY)
+        else:
+            print("all dead! in",turn,"turns")
+            print_grid(grid,turn)
+            break
 
 
-def play():
-    pass
+play_game(15)
